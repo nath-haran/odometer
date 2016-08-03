ed04f6134cc66dcafd0fad81643a435c1a66429d
Sivasubramanian <hsivasubramanian@paypalcorp.com>
2016-08-03 18:24:43 +0530

    test file added

---
+++ Test.java
@@ -0,0 +1,13 @@
+public class Test{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		System.out.println("5th next number of 1234");
+		System.out.println(Odometer.nNext(1234,5));
+		System.out.println("7th previous number of 689");
+		System.out.println(Odometer.nPrevious(689,7));
+		System.out.println("difference between 258 and 289");
+		System.out.println(Odometer.diff(258,289));
+		// your code goes here
+	}
+}
